6bbb883a1127ee7b145a47ed6be5a0b1dddea811
ihsan rezandy <irezandy@gmail.com>
2018-08-05 19:19:27 +0700

    upload exercise 4 switch-case

---
+++ exercise-4.js
@@ -0,0 +1,47 @@
+var tanggal = '21'; // assign nilai variabel tanggal disini!
+var bulan = '5'; // assign nilai variabel bulan disini! (dengan huruf awal kapital)
+var tahun = '2022'; // assign nilai variabel tahun disini!
+
+console.log(tanggal)
+
+switch (bulan) {
+  case '1':
+    console.log('Januari');
+    break;
+  case '2':
+    console.log('Februari');
+    break;
+  case '3':
+    console.log('Maret');
+    break;
+  case '4':
+    console.log('April');
+    break;
+  case '5':
+    console.log('Mei');
+    break;
+  case '6':
+    console.log('Juni');
+    break;
+  case '7':
+    console.log('Juli');
+    break;
+  case '8':
+    console.log('Agustus');
+    break;
+  case '9':
+    console.log('September');
+    break;
+  case '10':
+    console.log('Oktober');
+    break;
+  case '11':
+    console.log('November');
+    break;
+  case '12':
+    console.log('Desember');
+    break;
+  default:
+    console.log(null)
+}
+console.log(tahun)
