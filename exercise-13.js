baec803269a2ed304ef8771fdf9dce8a5f8bc3c8
ihsan rezandy <irezandy@gmail.com>
2018-08-05 23:04:44 +0700

    upload exercise 13 XO

---
+++ exercise-13.js
@@ -0,0 +1,17 @@
+var x = 1;
+var o = 0;
+function xo(str) {
+  if (length.x = length.o){
+    return true;
+  } else if (length.x < length.o){
+    return false;
+  } else (length.x > length.o){
+    return false;
+  }
+}
+// TEST CASES
+console.log(xo('xoxoxo')); // true
+console.log(xo('oxooxo')); // false
+console.log(xo('oxo')); // false
+console.log(xo('xxxooo')); // true
+console.log(xo('xoxooxxo')); // true
